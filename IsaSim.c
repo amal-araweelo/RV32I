1c9c69984eaee27f3e0200200b1206f8644f2d4a
Amal Araweelo Almis <124199007+amal-araweelo@users.noreply.github.com>
2023-11-16 13:03:21 +0100

    gjort kode pænere

--- IsaSim.c
+++ IsaSim.c
@@ -3,194 +3,198 @@
  * <p>
  * A tiny first step to get the simulator started. Can execute just a single
  * RISC-V instruction.
- * 
- * To run this program: 
+ *
+ * To run this program:
  *      mkdir build
  *      cd build
  *      cmake ..
  *      make
  *      ./isasim
  *
- * @author Martin Schoeberl (martin@jopdesign.com), Andrew Dobis (andrew.dobis@alumni.epfl.ch)
+ * @author Martin Schoeberl (martin@jopdesign.com), Andrew Dobis
+ * (andrew.dobis@alumni.epfl.ch)
  */
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 
 #define NO_ERR 0
 
-//Function Prototypes
+// Function Prototypes
 
-void ItypeSwitch(uint32_t funct3, uint32_t funct7, uint32_t rd, uint32_t rs1, uint32_t imm, uint32_t * reg);
-void RtypeSwitch(uint32_t funct3, uint32_t funct7, uint32_t rd, uint32_t rs1, uint32_t rs2, uint32_t* reg);
+void ItypeSwitch(uint32_t funct3, uint32_t funct7, uint32_t rd, uint32_t rs1,
+		 uint32_t imm, uint32_t *reg);
+void RtypeSwitch(uint32_t funct3, uint32_t funct7, uint32_t rd, uint32_t rs1,
+		 uint32_t rs2, uint32_t *reg);
 
 int main(void) {
-    uint32_t pc = 0;
-    uint32_t reg[32] = {0};
-    
-    uint32_t progr[3] = {
-        0x00200093, // addi x1 x0 2
-        0x00300113, // addi x2 x0 3
-        0x002081b3, // add x3 x1 x2
-    };
-    
-    printf("Hello RISC-V World!\n");
-    
-    while(1) {
-        uint32_t instr = progr[pc >> 2];
-        uint32_t opcode = instr & 0x7f;
-        uint32_t rd = (instr >> 7) & 0x01f;
-        uint32_t rs1 = (instr >> 15) & 0x01f;
-        uint32_t rs2 = (instr >> 20) & 0x01f;
-        uint32_t imm = (instr >> 20);
-        uint32_t funct3 = (instr >> 12) & 0x007;
-        uint32_t funct7 = (instr >> 25) & 0x07f; 
-        
-        switch (opcode) {
-            // I-type instructions
-            case 0x13:
-                ItypeSwitch(funct3, funct7, rd, rs1, imm, reg);
-                break;
-            case 0x33:
-                RtypeSwitch(funct3, funct7,rd, rs1, rs2, reg);
-                break;
-            default:
-                printf("Opcode %u not yet implemented\n", opcode);
-                break;
-        }
-        
-        pc += 4; 
-        
-        if ((pc >> 2) >= sizeof(progr) / sizeof(progr[0])) {
-            break;
-        }
-        
-        for (size_t i = 0; i < sizeof(reg) / sizeof(reg[0]); ++i) {
-            printf("%u ", reg[i]);
-        }
-        
-        printf("\n");
-    }
-    //TODO: Register dump
+	uint32_t pc = 0;
+	uint32_t reg[32] = {0};
 
-    printf("Program exit\n");
-    return NO_ERR;
+	uint32_t progr[3] = {
+	    0x00200093, // addi x1 x0 2
+	    0x00300113, // addi x2 x0 3
+	    0x002081b3, // add x3 x1 x2
+	};
+
+	printf("Hello RISC-V World!\n");
+
+	while (1) {
+		uint32_t instr = progr[pc >> 2];
+		uint32_t opcode = instr & 0x7f;
+		uint32_t rd = (instr >> 7) & 0x01f;
+		uint32_t rs1 = (instr >> 15) & 0x01f;
+		uint32_t rs2 = (instr >> 20) & 0x01f;
+		uint32_t imm = (instr >> 20);
+		uint32_t funct3 = (instr >> 12) & 0x007;
+		uint32_t funct7 = (instr >> 25) & 0x07f;
+
+		switch (opcode) {
+		// I-type instructions
+		case 0x13:
+			ItypeSwitch(funct3, funct7, rd, rs1, imm, reg);
+			break;
+		case 0x33:
+			RtypeSwitch(funct3, funct7, rd, rs1, rs2, reg);
+			break;
+		default:
+			printf("Opcode %u not yet implemented\n", opcode);
+			break;
+		}
+
+		pc += 4;
+
+		if ((pc >> 2) >= sizeof(progr) / sizeof(progr[0])) {
+			break;
+		}
+
+		for (size_t i = 0; i < sizeof(reg) / sizeof(reg[0]); ++i) {
+			printf("%u ", reg[i]);
+		}
+
+		printf("\n");
+	}
+	// TODO: Register dump
+
+	printf("Program exit\n");
+	return NO_ERR;
 }
-
 
 /* example function to handle both rs2 and imm
 int add(int isImm){
-	uint32_t a = rs1;
-	uint32_t b = rs2;
-	if (isImm == true){
-		b = imm;
-	}
+		uint32_t a = rs1;
+		uint32_t b = rs2;
+		if (isImm == true){
+				b = imm;
+		}
 
-	rd = a+b;
+		rd = a+b;
 }
 */
 
-void RtypeSwitch(uint32_t funct3, uint32_t funct7, uint32_t rd, uint32_t rs1, uint32_t rs2, uint32_t* reg){
-switch (funct3)
-{
-case 0x00:
-    if(funct7 != 0){
-        // rs1 - rs2 
-    }
-    else{
-        //add HAS IMM
-    }
-    break;
+void RtypeSwitch(uint32_t funct3, uint32_t funct7, uint32_t rd, uint32_t rs1,
+		 uint32_t rs2, uint32_t *reg) {
+	switch (funct3) {
+	case 0x00:
+		if (funct7 != 0) {
+			// rs1 - rs2
+		} else {
+			// add HAS IMM
+		}
+		break;
 
-case 0x01:
-    //sll HAS IMM
-    break;
+	case 0x01:
+		// sll HAS IMM
+		break;
 
-case 0x02:
-    //slt HAS IMM
-    break;
+	case 0x02:
+		// slt HAS IMM
+		break;
 
-case 0x03:
-    //sltu HAS IMM
-    break;
+	case 0x03:
+		// sltu HAS IMM
+		break;
 
-case 0x04:
-    //xor HAS IMM
-    break;
+	case 0x04:
+		// xor HAS IMM
+		break;
 
-case 0x05:
-    if(funct7 != 0){
-        //sra HAS IMM
-    }
-    else{
-        //srl HAS IMM
-    }
-    break;
+	case 0x05:
+		if (funct7 != 0) {
+			// sra HAS IMM
+		} else {
+			// srl HAS IMM
+		}
+		break;
 
-case 0x06:
-    //or HAS IMM
-    break;
+	case 0x06:
+		// or HAS IMM
+		break;
 
-case 0x07:
-    //and HAS IMM
-    break;
+	case 0x07:
+		// and HAS IMM
+		break;
 
-
-default:
-    printf("in RtypeSwitch funct3 error: case not defined");
-    printf("funct3 value (unsigned int): %u", funct3);
-    break;
-}
+	default:
+		printf("in RtypeSwitch funct3 error: case not defined");
+		printf("funct3 value (unsigned int): %u", funct3);
+		break;
+	}
 }
 
-void ItypeSwitch(uint32_t funct3, uint32_t funct7, uint32_t rd, uint32_t rs1, uint32_t imm, uint32_t * reg){
-    switch (funct3){
-        // addi
-        case 0x0:
-            reg[rd] = reg[rs1] + imm;
-        break;
-        
-        // slli
-        case 0x1:
-            reg[rd] = reg[rs1] << imm;
-        break;
+void ItypeSwitch(uint32_t funct3, uint32_t funct7, uint32_t rd, uint32_t rs1,
+		 uint32_t imm, uint32_t *reg) {
+	switch (funct3) {
+	// addi
+	case 0x0:
+		reg[rd] = reg[rs1] + imm;
+		break;
 
-        // slti
-        case 0x3:
-            if (reg[rs1] < imm) reg[rd] = 1 break;
-            reg[rd] = 0;
-        break;
+	// slli
+	case 0x1:
+		reg[rd] = reg[rs1] << imm;
+		break;
 
-        // xori
-        case 0x4:
-            reg[rd] = reg[rs1] ^ imm;
-        break;
+	// slti
+	case 0x3:
+		if (reg[rs1] < imm)
+			reg[rd] = 1;
+		break;
+		reg[rd] = 0;
+		break;
 
-        // srli and srai
-        case 0x5:
-            switch (funt7):
-                // srli
-                case 0x0:
-                    reg[rd] = reg[rs1] >> imm;
-                break;
-                // srai
-                case 0x20:
-                    if (reg[rs1] > 0) reg[rd] = reg[rs1] >> imm break;
-                    reg[rd] = (reg[rs1] >> imm) & 0xFFFFFFFF;
-                break;       
-        break;
-        
-        // ori
-        case 0x6:
-            reg[rd] = reg[rsl] | imm;
-        break;
+	// xori
+	case 0x4:
+		reg[rd] = reg[rs1] ^ imm;
+		break;
 
-        // andi
-        case 0x7:
-            reg[rd] = reg[rs1] & imm;
-        break;
+	// srli and srai
+	case 0x5:
+		switch (funct7) {
+		// srli
+		case 0x0:
+			reg[rd] = reg[rs1] >> imm;
+			break;
+		// srai
+		case 0x20:
+			if (reg[rs1] > 0)
+				reg[rd] = reg[rs1] >> imm;
+			break;
+			reg[rd] = (reg[rs1] >> imm) & 0xFFFFFFFF;
+			break;
+		}
+		break;
 
-        default:
-            printf("in ItypeSwitch error: case not defined");
-    }
+	// ori
+	case 0x6:
+		reg[rd] = reg[rs1] | imm;
+		break;
 
+	// andi
+	case 0x7:
+		reg[rd] = reg[rs1] & imm;
+		break;
+
+	default:
+		printf("in ItypeSwitch error: case not defined");
+	}
 }
--- src/example.c
+++ src/example.c
@@ -1,18 +1,17 @@
 #include "example.h"
 #include <stdlib.h>
 
-struct Example* Example_Create(int ex) {
-    struct Example* instance = (struct Example*)malloc(sizeof(struct Example));
-    if (instance) {
-        instance->ex = ex;
-    }
-    return instance;
+struct Example *Example_Create(int ex) {
+	struct Example *instance =
+	    (struct Example *)malloc(sizeof(struct Example));
+	if (instance) {
+		instance->ex = ex;
+	}
+	return instance;
 }
 
-int Example_GetEx(const struct Example* instance) {
-    return instance->ex;
-}
+int Example_GetEx(const struct Example *instance) { return instance->ex; }
 
-void Example_SetEx(struct Example* instance, int newEx) {
-    instance->ex = newEx;
+void Example_SetEx(struct Example *instance, int newEx) {
+	instance->ex = newEx;
 }
