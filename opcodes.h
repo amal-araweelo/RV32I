374441f67facf6154695801b1478201419b9d28f
Amal Araweelo Almis <124199007+amal-araweelo@users.noreply.github.com>
2023-11-27 09:11:59 +0100

    opdateret ignore

---
+++ opcodes.h
@@ -0,0 +1,17 @@
+#ifndef OPCODE_H
+#define OPCODE_H
+
+#include <stdint.h>
+
+// Opcodes - instructions
+#define LUI 0x37
+#define AUIPC 0x17
+#define JALR 0x67
+#define ECALL 0x73
+
+// Opcodes - instruction type
+#define I_TYPE 0x13
+#define R_TYPE 0x33
+#define SB_TYPE 0x63
+
+#endif // OPCODE_H
