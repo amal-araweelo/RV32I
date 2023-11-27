6258cd4823e14cd7cbbee3da9201f2e83a520efd
Trash-Child <dicte.mj.98@gmail.com>
2023-11-27 14:14:17 +0100

    fixed lhu, lw and sw

--- instructions.c
+++ instructions.c
@@ -211,7 +211,9 @@ void SBTypeSwitch(uint32_t funct3, uint32_t rs1, uint32_t rs2, int32_t imm, int3
 void STypeSwitch(uint32_t funct3, uint32_t rs1, uint32_t rs2, int32_t imm, int32_t *reg, int32_t *pc,
 		 int8_t *mem_base) {
 	int8_t *store_at = mem_base + (reg[rs1] + imm);
-	int8_t *store_at2 = mem_base + (reg[rs1] + imm+1);
+	int8_t *store_at1 = mem_base + (reg[rs1] + imm+1);
+	int8_t *store_at2 = mem_base + (reg[rs1] + imm+2);
+	int8_t *store_at3 = mem_base + (reg[rs1] + imm+3);
 
 	switch (funct3) {
 
@@ -233,19 +235,32 @@ void STypeSwitch(uint32_t funct3, uint32_t rs1, uint32_t rs2, int32_t imm, int32
 		uint16_t to_store_raw16 = reg[rs2] & 0x0000FFFF; // isolate lower 16 bits
 		printf("to_store_raw16 = %x \n", to_store_raw16);
 		*store_at = (uint8_t)(to_store_raw16 & 0x00FF);
-		*store_at2 = (uint8_t)(to_store_raw16 >> 8);
+		*store_at1 = (uint8_t)(to_store_raw16 >> 8);
 
-		uint16_t storedval_lo = *store_at;
-		uint16_t storedval_hi = *store_at2;
-
-		printf("stored %x at %p and -  %x at %p ", storedval_lo, store_at, storedval_hi, store_at2);
+		//	CHECKS
+		//uint16_t storedval_lo = *store_at;
+		//uint16_t storedval_hi = *store_at1;
+		//printf("stored %x at %p and -  %x at %p ", storedval_lo, store_at, storedval_hi, store_at2);
 		
 
 		break;
 
 	// sw
 	case 0x02:
-		*store_at = reg[rs2];
+		printf("hej fra sw \n");
+
+		printf("to_store_raw16 = %x \n", to_store_raw16);
+		*store_at = (uint8_t)(reg[rs2] & 0x000000FF);
+		*store_at1 = (uint8_t)((reg[rs2] >> 8) & 0x000000FF);
+		*store_at2 = (uint8_t)((reg[rs2] >> 16) & 0x000000FF);
+		*store_at3 = (uint8_t)((reg[rs2] >> 24) & 0x000000FF);
+
+		// CHECKS
+		//uint16_t storedval_0 = *store_at;
+		//uint16_t storedval_1 = *store_at1;
+		//uint16_t storedval_2 = *store_at2;
+		//uint16_t storedval_3 = *store_at3;
+		//printf("stored: \n %x at %p \n %x at %p \n %x at %p \n %x at %p ", storedval_0, store_at, storedval_1, store_at1, storedval_2, store_at2, storedval_3, store_at3);
 		break;
 
 	default:
@@ -257,7 +272,9 @@ void STypeSwitch(uint32_t funct3, uint32_t rs1, uint32_t rs2, int32_t imm, int32
 void ITypeLoadSwitch(uint32_t funct3, uint32_t funct7, uint32_t rd, uint32_t rs1, int32_t imm, int32_t *reg,
 		     int8_t *mem_base) {
 	int8_t *load_at = mem_base + (reg[rs1] + imm);
-	int8_t *load_at2 = mem_base + (reg[rs1] + imm + 1);
+	int8_t *load_at1 = mem_base + (reg[rs1] + imm + 1);
+	int8_t *load_at2 = mem_base + (reg[rs1] + imm + 2);
+	int8_t *load_at3 = mem_base + (reg[rs1] + imm + 3);
 
 	switch (funct3) {
 	// lb
@@ -276,28 +293,40 @@ void ITypeLoadSwitch(uint32_t funct3, uint32_t funct7, uint32_t rd, uint32_t rs1
 	case 0x01:
 		printf("hello from lh \n");
 		uint8_t to_load8_lo = *load_at;
-		uint8_t to_load8_hi = *load_at2;
+		uint8_t to_load8_hi = *load_at1;
 		
-		printf("to load LO: %x HI: %x \n", to_load8_lo, to_load8_hi);
+		//printf("to load LO: %x HI: %x \n", to_load8_lo, to_load8_hi);
 
 		uint16_t to_load16 = ( (to_load8_hi << 8) |
 							 (to_load8_lo)
 		);
 
-		printf("loaded tl16 with %x \n", to_load16);
+		//printf("loaded tl16 with %x \n", to_load16);
 
-		uint32_t to_load32 = to_load16; // load unsigned
-		printf("loaded tl32 with %x \n", to_load32);
+		uint32_t to_load32lh = to_load16; // load unsigned
+		//printf("loaded tl32 with %x \n", to_load32);
 
 		if ((to_load16 >> 15) == 1) { // sign extend if needed
-			to_load32 = to_load32 | 0xFFFF0000;
+			to_load32lh = to_load32lh | 0xFFFF0000;
 		}
-		reg[rd] = to_load32;
+		reg[rd] = to_load32lh;
 		break;
 
 	// lw
 	case 0x02:
-		reg[rd] = *load_at;
+		printf("hello from lw \n");
+		uint8_t to_load8_0 = *load_at;
+		uint8_t to_load8_1 = *load_at1;
+		uint8_t to_load8_2 = *load_at2;
+		uint8_t to_load8_3 = *load_at3;
+		
+
+		uint32_t to_load32lw = ( 	(to_load8_3 << 24) |
+								(to_load8_2 << 16) |
+								(to_load8_1 << 8) |
+							 	(to_load8_0)
+		);
+		reg[rd] = to_load32lw;
 		break;
 
 	// lbu
@@ -309,20 +338,20 @@ void ITypeLoadSwitch(uint32_t funct3, uint32_t funct7, uint32_t rd, uint32_t rs1
 	// lhu
 	case 0x05:
 		printf("hello from lhu \n");
-		uint8_t to_load8_lo = *load_at;
-		uint8_t to_load8_hi = *load_at2;
+		uint8_t to_load8_lolhu = *load_at;
+		uint8_t to_load8_hilhu = *load_at1;
 		
 		//printf("to load LO: %x HI: %x \n", to_load8_lo, to_load8_hi);
 
-		uint16_t to_load16 = ( (to_load8_hi << 8) |
-							 (to_load8_lo)
+		uint16_t to_load16lhu = ( (to_load8_hilhu << 8) |
+							 (to_load8_lolhu)
 		);
 
 		//printf("loaded tl16 with %x \n", to_load16);
-		uint32_t to_load32 = to_load16; // load unsigned
+		uint32_t to_load32lhu = to_load16lhu; // load unsigned
 		//printf("loaded tl32 with %x \n", to_load32);
 			
-		reg[rd] = to_load32;
+		reg[rd] = to_load32lhu;
 		
 		break;
 
