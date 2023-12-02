a0e440349baade9642e951dbd4bacc535de255b7
Amal Araweelo Almis <124199007+amal-araweelo@users.noreply.github.com>
2023-12-02 02:07:34 +0100

    cmake file deleted

--- CMakeFiles/isasim.dir/globals.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/isasim.dir/globals.c.o: \
- /mnt/c/Users/amaln/OneDrive\ -\ Danmarks\ Tekniske\ Universitet/3.\ Semester/Computerarkitektur/Final/globals.c \
- /usr/include/stdc-predef.h \
- /mnt/c/Users/amaln/OneDrive\ -\ Danmarks\ Tekniske\ Universitet/3.\ Semester/Computerarkitektur/Final/globals.h
